d3ca1e43ffe0f863524384af871a036129ed0ff0
faha1999 <kafahad1999@gmail.com>
2021-11-16 23:57:42 +0600

    innitial commit

---
+++ 6. More on Functions/app.js
@@ -0,0 +1,9 @@
+const startGameBtn = document.getElementById('start-game-btn');
+
+function startGame() {
+  console.log('game is starting.........');
+}
+
+// console.dir(startGame);
+
+startGameBtn.addEventListener('click, startGame');
