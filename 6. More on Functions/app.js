bb85125561a8a0c7b93b89db08b8eb7b418b4a99
faha1999 <kafahad1999@gmail.com>
2021-11-17 21:16:35 +0600

    added anonymous function

--- 6. More on Functions/app.js
+++ 6. More on Functions/app.js
@@ -1,9 +1,24 @@
 const startGameBtn = document.getElementById('start-game-btn');
 
-function startGame() {
-  console.log('game is starting.........');
-}
+const ROCK = 'ROCK';
+const PAPER = 'PAPER';
+const SCISSORS = 'SCISSORS';
+const DEFAULT_USER_CHOICE = ROCK;
 
-// console.dir(startGame);
+const getPlayerChoice = function () {
+  const selection = prompt(
+    `${ROCK}, ${PAPER} or ${SCISSORS}?`,
+    ''
+  ).toUpperCase();
+  if (selection !== ROCK && selection !== PAPER && selection !== SCISSORS) {
+    alert(`Invalid choice! We chose ${DEFAULT_USER_CHOICE} for you!`);
+    return DEFAULT_USER_CHOICE;
+  }
+  return selection;
+};
 
-startGameBtn.addEventListener('click, startGame');
+startGameBtn.addEventListener('click', function () {
+  console.log('Game is starting....');
+  const playerSelection = getPlayerChoice();
+  console.log(playerSelection);
+});
