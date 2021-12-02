6add4497346fb4d0a4728342f2ec262c3adeeed2
faha1999 <kafahad1999@gmail.com>
2021-12-02 22:29:00 +0600

    added filtering

--- 8. More on Arrays & Iterables/1. Introduction/app/script/app.js
+++ 8. More on Arrays & Iterables/1. Introduction/app/script/app.js
@@ -119,3 +119,9 @@ const sortedPrices = prices.sort((a, b) => {
 
 // console.log(sortedPrices.reverse());
 console.log(sortedPrices);
+
+const filteredArray = prices.filter((price, index, prices) => {
+  return price > 6;
+});
+
+console.log(filteredArray);
