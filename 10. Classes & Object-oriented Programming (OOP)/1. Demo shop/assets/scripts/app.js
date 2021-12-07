ed998b329178529bf1079a7168a98b0edb73792e
faha1999 <kafahad1999@gmail.com>
2021-12-07 21:31:40 +0600

    applied constructor method

--- 10. Classes & Object-oriented Programming (OOP)/1. Demo shop/assets/scripts/app.js
+++ 10. Classes & Object-oriented Programming (OOP)/1. Demo shop/assets/scripts/app.js
@@ -1,21 +1,34 @@
+class product {
+  title = 'DEFAULT';
+  imageUrl;
+  description;
+  price;
+
+  constructor(title, image, desc, price) {
+    this.title = title;
+    this.imageUrl = image;
+    this.description = desc;
+    this.price = price;
+  }
+}
+
 const productsList = {
   products: [
-    {
-      title: 'A Pillow',
-      imageUrl:
-        'https://images.unsplash.com/photo-1584100931218-4c2a2986c251?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',
-      price: 19.99,
-      description: 'A super soft pillow!'
-    },
+    new product(
+      'A Pillow',
+      'https://images.unsplash.com/photo-1584100931218-4c2a2986c251?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',
+      'A super soft pillow!',
+      19.99
+    ),
 
-    {
-      title: 'A Carpet',
-      imageUrl:
-        'https://images.unsplash.com/photo-1600166898405-da9535204843?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Y2FycGV0fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
-      price: 79.99,
-      description: 'A super soft carpet you might like!'
-    }
+    new product(
+      'A Carpet',
+      'https://images.unsplash.com/photo-1600166898405-da9535204843?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Y2FycGV0fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
+      'A super soft carpet you might like!',
+      79.99
+    )
   ],
+
   render() {
     const renderHook = document.getElementById('app');
     const prodList = document.createElement('ul');
