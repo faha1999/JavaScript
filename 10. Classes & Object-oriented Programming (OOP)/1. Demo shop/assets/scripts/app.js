5f492fb071221d877b23a624a2764cf18e6d4ac8
faha1999 <kafahad1999@gmail.com>
2021-12-09 00:36:36 +0600

    completed mini-shop

--- 10. Classes & Object-oriented Programming (OOP)/1. Demo shop/assets/scripts/app.js
+++ 10. Classes & Object-oriented Programming (OOP)/1. Demo shop/assets/scripts/app.js
@@ -12,14 +12,96 @@ class product {
   }
 }
 
-class ProductItem {
-  constructor(product) {
-    this.product = product;
+class ElementAttribute {
+  constructor(attrName, attrValue) {
+    this.name = attrName;
+    this.value = attrValue;
+  }
+}
+
+class Component {
+  constructor(renderHookId, shouldRender = true) {
+    this.hookId = renderHookId;
+    if (shouldRender) {
+      this.render();
+    }
+  }
+
+  render() {}
+
+  crateRootElement(tag, cssClasses, attributes) {
+    const rootElement = document.createElement(tag);
+    if (cssClasses) {
+      rootElement.className = cssClasses;
+    }
+    if (attributes && attributes.length > 0) {
+      for (const attr of attributes) {
+        rootElement.setAttribute(attr.name, attr.value);
+      }
+    }
+    document.getElementById(this.hookId).append(rootElement);
+    return rootElement;
+  }
+}
+
+class ShoppingCart extends Component {
+  items = [];
+
+  set cartItems(value) {
+    this.items = value;
+    this.totalOutput.innerHTML = `<h2>Total: \$${this.totalAmount.toFixed(
+      2
+    )}</h2>`;
+  }
+
+  get totalAmount() {
+    const sum = this.items.reduce(
+      (prevValue, curItem) => prevValue + curItem.price,
+      0
+    );
+    return sum;
+  }
+
+  constructor(renderHookId) {
+    super(renderHookId);
+  }
+
+  addProduct(product) {
+    const updatedItems = [...this.items];
+    updatedItems.push(product);
+    this.cartItems = updatedItems;
+  }
+
+  orderProducts() {
+    console.log('Ordering...');
+    console.log(this.items);
   }
 
   render() {
-    const prodEL = document.createElement('li');
-    prodEL.classList = 'product-item';
+    const cartEl = this.crateRootElement('section', 'cart');
+    cartEl.innerHTML = `
+    <h2>Total: \$${0}</h2>
+    <button>Order Now!</button>
+    `;
+    const orderButton = cartEl.querySelector('button');
+    orderButton.addEventListener('click', () => this.orderProducts());
+    this.totalOutput = cartEl.querySelector('h2');
+  }
+}
+
+class ProductItem extends Component {
+  constructor(product, renderHookId) {
+    super(renderHookId, false);
+    this.product = product;
+    this.render();
+  }
+
+  addToCart() {
+    App.addProductToCart(this.product);
+  }
+
+  render() {
+    const prodEL = this.crateRootElement('li', 'product-item');
     prodEL.innerHTML = `
       <div>
         <img src="${this.product.imageUrl}" alt="${this.product.title}">
@@ -31,40 +113,76 @@ class ProductItem {
         </div>
         </div>
       `;
+    const addCartButton = prodEL.querySelector('button');
+    addCartButton.addEventListener('click', this.addToCart.bind(this));
     return prodEL;
   }
 }
 
-class ProductList {
-  products = [
-    new product(
-      'A Pillow',
-      'https://images.unsplash.com/photo-1584100931218-4c2a2986c251?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',
-      'A super soft pillow!',
-      19.99
-    ),
-    new product(
-      'A Carpet',
-      'https://images.unsplash.com/photo-1600166898405-da9535204843?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Y2FycGV0fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
-      'A super soft carpet you might like!',
-      79.99
-    )
-  ];
+class ProductList extends Component {
+  products = [];
 
-  constructor() {}
+  constructor(renderHookId) {
+    super(renderHookId);
+    this.fetchProducts();
+  }
+
+  fetchProducts() {
+    this.products = [
+      new product(
+        'A Pillow',
+        'https://images.unsplash.com/photo-1584100931218-4c2a2986c251?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',
+        'A super soft pillow!',
+        19.99
+      ),
+      new product(
+        'A Carpet',
+        'https://images.unsplash.com/photo-1600166898405-da9535204843?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Y2FycGV0fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
+        'A super soft carpet you might like!',
+        79.99
+      )
+    ];
+    this.renderProducts();
+  }
+
+  renderProducts() {
+    for (const prod of this.products) {
+      new ProductItem(prod, 'prod-list');
+    }
+  }
 
   render() {
-    const renderHook = document.getElementById('app');
-    const prodList = document.createElement('ul');
-    prodList.classList = 'product-list';
-    for (const prod of this.products) {
-      const productItem = new ProductItem(prod);
-      const prodEL = productItem.render();
-      prodList.append(prodEL);
+    this.crateRootElement('ul', 'product-list', [
+      new ElementAttribute('id', 'prod-list')
+    ]);
+    if (this.products && this.products.length > 0) {
+      this.renderProducts();
     }
-    renderHook.append(prodList);
   }
 }
 
-const productList = new ProductList();
-productList.render();
+class Shop {
+  constructor() {
+    this.render();
+  }
+
+  render() {
+    this.cart = new ShoppingCart('app');
+    new ProductList('app');
+  }
+}
+
+class App {
+  static cart;
+
+  static init() {
+    const shop = new Shop();
+    this.cart = shop.cart;
+  }
+
+  static addProductToCart(product) {
+    this.cart.addProduct(product);
+  }
+}
+
+App.init();
