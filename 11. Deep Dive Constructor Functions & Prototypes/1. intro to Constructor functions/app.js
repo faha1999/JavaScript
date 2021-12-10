3762a1b26e8da847324928ab626d44149f5281d2
faha1999 <kafahad1999@gmail.com>
2021-12-10 19:22:35 +0600

    completed Classes & prototype

--- 11. Deep Dive Constructor Functions & Prototypes/1. intro to Constructor functions/app.js
+++ 11. Deep Dive Constructor Functions & Prototypes/1. intro to Constructor functions/app.js
@@ -63,25 +63,59 @@
 //   }
 // }
 
-function Person() {
-  this.age = 30;
-  this.name = 'Max';
-  this.greet = function () {
+// // start prototype chain & global object
+
+// function Person() {
+//   this.age = 30;
+//   this.name = 'Max';
+//   this.greet = function () {
+//     console.log(
+//       'Hi, I am ' + this.name + ' and I am ' + this.age + ' years old.'
+//     );
+//   };
+// }
+
+// Person.describe = function () {
+//   console.log('Creating persons...');
+// };
+
+// Person.prototype.printAge = function () {
+//   console.log(this.age);
+// };
+
+// console.dir(Person);
+
+// const p = new Person();
+// p.greet();
+// p.printAge();
+// console.log(p.length);
+// console.log(p.toString());
+// console.log(p.__proto__);
+// const p2 = new p.__proto__.constructor();
+// console.dir(Object.prototype);
+
+// // Classes & prototype
+
+class AgedPerson {
+  printAge() {
+    console.log(this.age);
+  }
+}
+
+class person extends AgedPerson {
+  name = 'Max';
+
+  constructor() {
+    super();
+    this.age = 30;
+  }
+
+  greet() {
     console.log(
       'Hi, I am ' + this.name + ' and I am ' + this.age + ' years old.'
     );
-  };
+  }
 }
 
-Person.prototype.printAge = function () {
-  console.log(this.age);
-};
-
-console.dir(Person);
-
-const p = new Person();
-p.greet();
-p.printAge;
-console.log(p.__proto__);
-const p2 = new p.__proto__.constructor();
-console.log(p2);
+const p = new person();
+console.log(p);
