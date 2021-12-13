b3f61cd90b9384cababe9a69938d1a9cfa0854b7
faha1999 <kafahad1999@gmail.com>
2021-12-13 23:13:45 +0600

    added new btn & logic

--- 14. Working with Events/1. Event intro/assets/scripts/events.js
+++ 14. Working with Events/1. Event intro/assets/scripts/events.js
@@ -1,30 +1,55 @@
-const button = document.querySelector('button');
+// const button = document.querySelector('button');
 
-// button.onclick = function() {
+// // button.onclick = function() {
 
-// }
+// // }
 
-const buttonClickHandler = () => {
-  alert('Button was clicked!');
+// const buttonClickHandler = () => {
+//   alert('Button was clicked!');
+// };
+
+// const anotherButtonClickHandler = () => {
+//   console.log('Button was clicked!');
+// };
+
+// // button.onclick = buttonClickHandler;
+// // button.onclick = anotherButtonClickHandler;
+
+// // button.addEventListener('click', buttonClickHandler);
+
+// // setTimeout(() => {
+// //   button.removeEventListener('click', buttonClickHandler);
+// // }, 2000);
+
+// const boundFn = buttonClickHandler.bind(this);
+
+// button.addEventListener('click', boundFn);
+
+// setTimeout(() => {
+//   button.removeEventListener('click', boundFn);
+// });
+
+// // the event object
+
+const buttons = document.querySelectorAll('button');
+
+const buttonClickHandler = (event) => {
+  event.target.disabled = true;
+  console.log(event);
 };
 
 const anotherButtonClickHandler = () => {
   console.log('Button was clicked!');
 };
 
-// button.onclick = buttonClickHandler;
-// button.onclick = anotherButtonClickHandler;
+const boundFn = buttonClickHandler.bind(this);
 
 // button.addEventListener('click', buttonClickHandler);
 
 // setTimeout(() => {
 //   button.removeEventListener('click', buttonClickHandler);
-// }, 2000);
+// });
 
-const boundFn = buttonClickHandler.bind(this);
-
-button.addEventListener('click', boundFn);
-
-setTimeout(() => {
-  button.removeEventListener('click', boundFn);
+buttons.forEach((btn) => {
+  btn.addEventListener('click', buttonClickHandler);
 });
