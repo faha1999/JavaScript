80c521757e4dfb4e1936e79c64d352885280ede7
faha1999 <kafahad1999@gmail.com>
2021-12-20 19:52:01 +0600

    tested recursion

--- 15. Advanced Function Concepts/1. Advance function/app.js
+++ 15. Advanced Function Concepts/1. Advance function/app.js
@@ -69,3 +69,26 @@ let name = 'John Dou';
 username = 'Dou';
 
 greetUser();
+
+////Start recursion
+
+// function powerOf(x, n) {
+//   let result = 1;
+
+//   for (let i = 0; i < n; i++) {
+//     result *= x;
+//   }
+
+//   return result;
+// }
+
+function powerOf(x, n) {
+  // if (n === 1) {
+  //   return x;
+  // }
+  // return x * powerOf(x, n - 1);
+
+  return n === 1 ? x : x * powerOf(x, n - 1);
+}
+
+console.log(powerOf(2, 3)); // 2*2*2
