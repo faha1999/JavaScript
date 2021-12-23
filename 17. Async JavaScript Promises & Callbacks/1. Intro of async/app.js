4fe9064d93d19a509f84407e3096dd64873c9719
faha1999 <kafahad1999@gmail.com>
2021-12-23 20:49:35 +0600

    tested promises

--- 17. Async JavaScript Promises & Callbacks/1. Intro of async/app.js
+++ 17. Async JavaScript Promises & Callbacks/1. Intro of async/app.js
@@ -1,20 +1,29 @@
 const button = document.querySelector('button');
 const output = document.querySelector('p');
 
+const setTimer = (duration) => {
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve('Done!');
+    }, duration);
+  });
+  return promise;
+};
+
 function trackUserHandler() {
   navigator.geolocation.getCurrentPosition(
     (posData) => {
-      setTimeout(() => {
-        console.log(posData);
-      }, 2000);
+      setTimer(2000).then((data) => {
+        console.log(data, posData);
+      });
     },
     (error) => {
       console.log(error);
     }
   );
-  setTimeout(() => {
+  setTimer(1000).then(() => {
     console.log('Timer done!');
-  }, 0);
+  });
   console.log('Getting position ...........');
 }
 
