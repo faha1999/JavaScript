98b5a823cde3ded16ff622f4b788a2e44ac4de84
faha1999 <kafahad1999@gmail.com>
2021-12-23 20:34:39 +0600

    applied callback & settimeout() function

--- 17. Async JavaScript Promises & Callbacks/1. Intro of async/app.js
+++ 17. Async JavaScript Promises & Callbacks/1. Intro of async/app.js
@@ -4,12 +4,17 @@ const output = document.querySelector('p');
 function trackUserHandler() {
   navigator.geolocation.getCurrentPosition(
     (posData) => {
-      console.log(posData);
+      setTimeout(() => {
+        console.log(posData);
+      }, 2000);
     },
     (error) => {
       console.log(error);
     }
   );
+  setTimeout(() => {
+    console.log('Timer done!');
+  }, 0);
   console.log('Getting position ...........');
 }
 
