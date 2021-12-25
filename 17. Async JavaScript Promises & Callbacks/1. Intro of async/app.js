177f6c9ae9b5ee9cc1c6050cff89e84f003de371
faha1999 <kafahad1999@gmail.com>
2021-12-25 21:11:17 +0600

    applied async, await & error handling

--- 17. Async JavaScript Promises & Callbacks/1. Intro of async/app.js
+++ 17. Async JavaScript Promises & Callbacks/1. Intro of async/app.js
@@ -27,8 +27,15 @@ const setTimer = (duration) => {
 
 async function trackUserHandler() {
   //let positionData;
-  const posData = await getPosition();
-  const timerData = await setTimer(2000);
+
+  let posData;
+  let timerData;
+  try {
+    posData = await getPosition();
+    timerData = await setTimer(2000);
+  } catch (error) {
+    console.log(error);
+  }
   console.log(timerData, posData);
   // .then((posData) => {
   //   positionData = posData;
