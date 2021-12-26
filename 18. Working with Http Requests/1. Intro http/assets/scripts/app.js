2fb672e8fa07ab53a66b47a8f09894a3835bb008
faha1999 <kafahad1999@gmail.com>
2021-12-26 17:17:54 +0600

    applied promise

--- 18. Working with Http Requests/1. Intro http/assets/scripts/app.js
+++ 18. Working with Http Requests/1. Intro http/assets/scripts/app.js
@@ -1,24 +1,37 @@
 const listElement = document.querySelector('.posts');
 const postTemplate = document.getElementById('single-post');
 
-const xhr = new XMLHttpRequest();
+function sendHttpRequest(method, url) {
+  const promise = new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
 
-xhr.open('GET', 'https://jsonplaceholder.typicode.com/posts');
+    xhr.open(method, url);
 
-xhr.responseType = 'json';
+    xhr.responseType = 'json';
 
-xhr.onload = function() {
-  // const listOfPosts = JSON.parse(xhr.response);
-  const listOfPosts = xhr.response;
+    xhr.onload = function () {
+      resolve(xhr.response);
+      // const listOfPosts = JSON.parse(xhr.response);
+    };
+
+    xhr.send();
+  });
+
+  return promise;
+}
+
+async function fetchPosts() {
+  const responseData = await sendHttpRequest(
+    'GET',
+    'https://jsonplaceholder.typicode.com/posts'
+  );
+  const listOfPosts = responseData;
   for (const post of listOfPosts) {
     const postEl = document.importNode(postTemplate.content, true);
     postEl.querySelector('h2').textContent = post.title.toUpperCase();
     postEl.querySelector('p').textContent = post.body;
     listElement.append(postEl);
   }
-};
+}
 
-xhr.send();
-
-
-
+fetchPosts();
