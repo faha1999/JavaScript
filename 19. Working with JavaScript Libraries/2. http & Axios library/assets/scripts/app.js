6d4a1d907ce08400c6c5850263d20252d2118804
faha1999 <kafahad1999@gmail.com>
2021-12-31 19:26:18 +0600

    applied axios library

--- 19. Working with JavaScript Libraries/2. http & Axios library/assets/scripts/app.js
+++ 19. Working with JavaScript Libraries/2. http & Axios library/assets/scripts/app.js
@@ -60,11 +60,10 @@ function sendHttpRequest(method, url, data) {
 
 async function fetchPosts() {
   try {
-    const responseData = await sendHttpRequest(
-      'GET',
+    const response = await axios.get(
       'https://jsonplaceholder.typicode.com/posts'
     );
-    const listOfPosts = responseData;
+    const listOfPosts = response.data;
     for (const post of listOfPosts) {
       const postEl = document.importNode(postTemplate.content, true);
       postEl.querySelector('h2').textContent = post.title.toUpperCase();
@@ -74,6 +73,7 @@ async function fetchPosts() {
     }
   } catch (error) {
     alert(error.message);
+    console.log(error.response);
   }
 }
 
@@ -90,7 +90,11 @@ async function createPost(title, content) {
   // fd.append('body', content);
   fd.append('userId', userId);
 
-  sendHttpRequest('POST', 'https://jsonplaceholder.typicode.com/posts', fd);
+  const response = await axios.post(
+    'https://jsonplaceholder.typicode.com/posts',
+    fd
+  );
+  console.log(response);
 }
 
 fetchButton.addEventListener('click', fetchPosts);
@@ -105,9 +109,6 @@ form.addEventListener('submit', (event) => {
 postList.addEventListener('click', (event) => {
   if (event.target.tagName === 'BUTTON') {
     const postId = event.target.closest('li').id;
-    sendHttpRequest(
-      'DELETE',
-      `https://jsonplaceholder.typicode.com/posts/${postId}`
-    );
+    axios.delete(`https://jsonplaceholder.typicode.com/posts/${postId}`);
   }
 });
